5588607004ae571f2007a6ffd1a6eb86d3cf4e64
WHB901 <abdulwhbhussain@gmail.com>
2023-08-02 14:33:44 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Details from './components/Details';
+import SearchPage from './components/SearchPage';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">  
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:name/:lat/:lon" element={<Details />} />
+        <Route path="/search/:name" element={<SearchPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
